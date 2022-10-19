5f0d078f611eb0ec106b0cf68859cf56f4a8d11d
MurituMaina <vincent.maina@student.moringaschool.com>
2022-10-19 23:59:26 +0300

    complete tasks

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,35 @@
+class Book
+    attr_writer :title
+    attr_writer :author
+    attr_writer :page_count
+    attr_writer :genre
+    attr_accessor :turn_page
+def initialize(title)
+    @title = title
+    # @author = author
+
+end
+
+def title 
+    @title 
+  end
+def author 
+    @author
+ 
+end
+
+def page_count 
+    @page_count
+end
+
+def  genre
+    @genre 
+end
+
+def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
 
 
+
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,36 @@
 # Make your shoe class here!
+class Shoe
+    attr_writer :brand
+    attr_writer :color
+    attr_writer :size
+    attr_writer :material
+    attr_writer :condition
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def brand
+        @brand
+    end
+
+    def color
+        @color
+    end
+    def size 
+        @size
+    end
+    def material
+        @material
+    end
+    def condition
+        @condition
+    end
+
+    def cobble
+       
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+        puts 'makes the shoe\'s condition new'
+
+    end
+end
